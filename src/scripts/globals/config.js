a8586bc87ee6b189397b236022bee77caa6cd968
bahtiaranam <bahtiaranam07@gmail.com>
2024-09-17 04:04:53 +0700

    create pwa with native and workbox

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '7bd96037709e4f67380e0afa84948fc2',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
